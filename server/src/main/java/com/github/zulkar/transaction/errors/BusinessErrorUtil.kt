d6f229bb96108c9bb0799e434a6344eac5b7e8b7
Alex Bubenchikov <zulkar@yandex.ru>
2019-09-30 00:12:32 +0300

    added logging to business utils

--- server/src/main/java/com/github/zulkar/transaction/errors/BusinessErrorUtil.kt
+++ server/src/main/java/com/github/zulkar/transaction/errors/BusinessErrorUtil.kt
@@ -1,35 +1,41 @@
 package com.github.zulkar.transaction.errors
 
 import com.github.zulkar.transaction.model.User
+import org.apache.logging.log4j.LogManager
 import java.math.BigDecimal
 
 object BusinessErrorUtil {
 
+    val logger = LogManager.getLogger(BusinessErrorUtil.javaClass)
     @JvmStatic
     fun throwAlreadyExistsException(user: User) {
+        logger.info("User {} already exists", user)
         throw BusinessException(1, "User ${user.name} already exists")
     }
 
     @JvmStatic
     fun throwUserNotExists(user: User?) {
+        logger.info("User {} not exists", user)
         throw BusinessException(2, "User ${user?.name ?: ""} not exists")
     }
 
-    @Suppress("UNUSED_PARAMETER")
     @JvmStatic
     fun throwTransferAmountShouldBePositive(amount: BigDecimal?) {
+        logger.info("amount should be positive but was {}", amount)
         throw BusinessException(3, "Transfer amount should be positive")
 
     }
 
     @JvmStatic
     fun throwNotEnoughMoney(user: User, amount: BigDecimal) {
+        logger.info("User {} has not enough money to perform operation with amount {}", user, amount)
         throw BusinessException(4, "We Require More Minerals. User ${user.name} does not have ${amount}")
     }
 
-    @Suppress("UNUSED_PARAMETER")
     @JvmStatic
-    fun throwCannotTransferSelf(from: User) {
+    fun throwCannotTransferSelf(user: User) {
+        logger.info("User {} trying to transfer to self", user)
         throw BusinessException(5, "Cannot transfer to self")
     }
+
 }
