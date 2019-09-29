a397aef8e5de9bdf892ec5e618099588f20653d4
Alex Bubenchikov <zulkar@yandex.ru>
2019-09-30 00:03:40 +0300

    added transaction logging

--- server/src/main/java/com/github/zulkar/transaction/processing/ProcessingServiceImpl.java
+++ server/src/main/java/com/github/zulkar/transaction/processing/ProcessingServiceImpl.java
@@ -3,6 +3,8 @@ package com.github.zulkar.transaction.processing;
 import com.github.zulkar.transaction.errors.BusinessErrorUtil;
 import com.github.zulkar.transaction.model.Balance;
 import com.github.zulkar.transaction.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
@@ -11,6 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class ProcessingServiceImpl implements ProcessingService {
+    private final Logger TRANSACTION_LOG = LogManager.getLogger("TRANSACTION");
     private ConcurrentHashMap<User, Balance> database = new ConcurrentHashMap<>();
 
     @Override
@@ -18,13 +21,14 @@ public class ProcessingServiceImpl implements ProcessingService {
         if (database.putIfAbsent(user, new Balance()) != null) {
             BusinessErrorUtil.throwAlreadyExistsException(user);
         }
-
+        TRANSACTION_LOG.info("Account {} created", user);
     }
 
     @Override
     public BigDecimal replenish(@NotNull User user, @NotNull BigDecimal amount) {
         validateUserActive(user);
         validateAmountPositive(amount);
+        TRANSACTION_LOG.info("Account {} replenished with {}", user, amount);
         return database.get(user).add(amount);
     }
 
@@ -37,10 +41,12 @@ public class ProcessingServiceImpl implements ProcessingService {
 
         Balance fromBalance = database.get(from);
         Balance toBalance = database.get(to);
-
+        TRANSACTION_LOG.info("trying to transfer {} from {} to {}", amount, from, to);
         if (fromBalance.tryBlock(amount)) {
             toBalance.add(amount);
+            TRANSACTION_LOG.info("transferred {} from {} to {}", amount, from, to);
         } else {
+            TRANSACTION_LOG.info("transfer {} from {} to {} failed", amount, from, to);
             BusinessErrorUtil.throwNotEnoughMoney(from, amount);
         }
     }
