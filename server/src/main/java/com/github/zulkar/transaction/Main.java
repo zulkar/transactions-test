372d324dfd874a07230382916297cb67094911df
Alex Bubenchikov <zulkar@yandex.ru>
2019-09-30 00:49:07 +0300

    parse port number

--- server/src/main/java/com/github/zulkar/transaction/Main.java
+++ server/src/main/java/com/github/zulkar/transaction/Main.java
@@ -1,6 +1,10 @@
 package com.github.zulkar.transaction;
 
+import org.apache.commons.cli.*;
+
 public class Main {
+    private static final int DEFAULT_PORT = 8090;
+
     public static void main(String[] args) throws Exception {
         int port = parsePort(args);
         TransactionServer transactionServer = new TransactionServer(port);
@@ -8,6 +12,21 @@ public class Main {
     }
 
     private static int parsePort(String[] args) {
-        return 8090;
+        Options options = new Options();
+        options.addOption("p", "port", true, "port to start");
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            String port = cmd.getOptionValue("p");
+            if (port == null) {
+                return DEFAULT_PORT;
+            }
+            return Integer.parseInt(port);
+        } catch (ParseException | NumberFormatException e) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("transactions test", options);
+            System.exit(1);
+            return -1;
+        }
     }
 }
